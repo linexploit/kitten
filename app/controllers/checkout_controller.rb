d9f1c7240f7c5d52d0e1efe96e962c3a3eb62831
linexploit <linexploit@protonmail.com>
2024-05-28 17:30:03 +0100

    effective stripe

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,37 @@
+class CheckoutController < ApplicationController
+  def create
+    @total = params[:total].to_d
+    @order_id = params[:order_id]
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            unit_amount: (@total*100).to_i,
+            product_data: {
+              name: 'Rails Stripe Checkout',
+            },
+          },
+          quantity: 1
+        }
+      ],
+      metadata: {
+        order_id: @order_id
+      },
+      mode: 'payment',
+      success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: checkout_cancel_url
+    )
+    redirect_to @session.url, allow_other_host: true
+  end
+
+  def success
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    @order_id = @session.metadata.order_id
+  end
+
+  def cancel
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,13 @@ Rails.application.routes.draw do
     post 'add_item/:item_id', to: 'carts#add_item', as: 'add_item'
     post 'confirm_order', to: 'carts#confirm_order', as: 'confirm_order'
   end
+
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+  end
+
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 end
